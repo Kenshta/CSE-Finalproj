49e2548b9826386b88f5eba9fdc92f7abfae05a4
Kenshta <jayelbatoctoy@gmail.com>
2025-12-14 17:42:32 +0800

    done working all

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'your_mysql_password'  # ← Update this
+    MYSQL_PASSWORD = 'root'  
     MYSQL_DB = 'shoes_db'
